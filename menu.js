bbfba45109f97c909e813ed52a497557ed7e455f
Jaun190 <jaun190@gmail.com>
2025-06-26 21:54:24 +0200

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,4 @@
+document.getElementById("menuToggle").addEventListener("click", () => {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.style.display = sidebar.style.display === "block" ? "none" : "block";
+});
